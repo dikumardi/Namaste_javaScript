5ffdce8536c349e4bc6d6ff9749c702c4a95e8bd
Diku Mardi <dikumardi047@gmail.com>
2025-08-05 14:09:13 +0530

    undefined vs not defined

---
+++ 06_Undefined_Vs_Not_defined/script.js
@@ -0,0 +1,24 @@
+ 
+ var a ;
+ a = 22 ;
+ console.log(a);
+ 
+  if (a === undefined) {
+    console.log(" a is undefined");
+  }
+  else{
+    console.log("a is not undefined");
+    
+  }
+
+  /**Loosely typed language or weakly type language means we can change variable type without any error */
+  var x ;
+  console.log(x);
+   x = 21 ;
+   console.log(x);
+   x = "hello world" ;
+   console.log(x);
+   
+   console.log(y);
+   
+  

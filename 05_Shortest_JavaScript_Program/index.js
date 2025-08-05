4641599350b13d2bb97aed022b669546c345d21c
Diku Mardi <dikumardi047@gmail.com>
2025-08-05 10:10:39 +0530

    shortest js program

--- 05_Shortest_JavaScript_Program/index.js
+++ 05_Shortest_JavaScript_Program/index.js
@@ -1,5 +1,3 @@
-
- 
 let a =10;
 var b = 90 ;
 const c = 4 ;
@@ -14,22 +12,5 @@ console.log(b); //  attach to window
 console.log(c);// not attach to window
 console.log(x);// not attach to window
 console.log(y); //not attach to window
-
-/*An empty JS file is still a valid program because JavaScript automatically creates a Global Execution Context (GEC).
-
-In browsers, the global object is called the window object.
-
-In the global scope, this === window.
-
-Only var declarations and functions are added to the window object.
-
-let and const are global, but not added to the window object.
-
-Variables declared inside a function (var, let, or const) are local and cannot be accessed outside the function.
-
-In Node.js, the global object is called global, not window.
-
- */
-
 
 

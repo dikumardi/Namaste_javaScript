19a8e2e57f26e3cfb748c40bf118822742f6ba0f
Diku Mardi <dikumardi047@gmail.com>
2025-08-07 22:59:34 +0530

    closures in js

--- 10_Closures_in_JavaScript/script.js
+++ 10_Closures_in_JavaScript/script.js
@@ -1,21 +1,15 @@
-// function x() {
-//     var a = 7;
-//     function y() {
-//         console.log(a);
+function x() {
+    var a = 7;
+    function y() {
+        console.log(a);
         
         
-//     }
-//     y();
+    }
+    y();
     
-// }
-// x();
+}
+x();
 
-/*jab function k  */
-
-/**we can assign function to a variable 
- * and we can pass function in an  argument
- * we cab retur function eg below
- */
 
 // function x() {
 //     var a = 7;
@@ -28,7 +22,8 @@
 // var z = x();
 // console.log(z);
 // z();
-/**yaha pe y ko return kar rahe hai to ye  pura function x() me return ho jayega isko save karne k liye hum ise z m save kar rahe hai karne k bad hum iska function ko call kar rahe hai call karne k bad ye console log me jo a hai iska value print ho jata hai qki ye closures hai jo ki function k andar hai aur lexical environment hai or hum isko ase v lik sakte hai 
+
+/**when return function  
 function x() {
     var a = 7;
    return function y() {
@@ -39,10 +34,12 @@ function x() {
 var z = x();
 console.log(z);
 z(); 
+ */
 
 
 
-*/
+
+/*
 
 function x() {
     var a = 7;
@@ -56,5 +53,4 @@ function x() {
 var z = x();
 console.log(z);
 z(); 
-
-/**upara wala code sab same hai likin ye a=100 updation ho raha hai iska matalab phele a ka value local environment m 7 tha bad m iska value update ho k 100 hogya hai aur ye same work karta hai above se*/
+ */

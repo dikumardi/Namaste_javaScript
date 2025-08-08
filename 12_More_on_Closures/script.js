0e45fb6d80e8ed18af3c5a7f19768366e80946b7
Diku Mardi <dikumardi047@gmail.com>
2025-08-08 23:28:25 +0530

    closures

---
+++ 12_More_on_Closures/script.js
@@ -0,0 +1,15 @@
+function outest() {
+    let c = 21 ;
+    function outer(b) {
+     function inner() {
+        console.log(a,b,c);
+        
+    }
+        let a  = 43 ;
+return inner;
+}
+return outer 
+}
+// outer()();
+var ans = outest()("hello");
+ans();
